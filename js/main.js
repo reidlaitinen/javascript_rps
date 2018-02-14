925c5173c0a5febefb273ff06ec0f63723b9a225
Reid Laitinen <reidlaitinen@gmail.com>
2018-02-13 21:46:55 -0700

    add image functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* Might use these functions later, for reasons.
+
 function tieGame() {
   console.log("tie game")
 }
@@ -9,28 +11,36 @@ function userWin() {
 function computerWin() {
   console.log("you lose!")
 }
+*/
+function showComputerChoiceImg(id) {
+  document.getElementById('computer-paper-img').classList.add('displayOff');
+  document.getElementById('computer-rock-img').classList.add('displayOff');
+  document.getElementById('computer-scissors-img').classList.add('displayOff');
+  document.getElementById('computer-'+id+'-img').classList.remove('displayOff');
+}
 
-function displayResult(winner) {
+function displayResult(winner, userChoice, computerChoice) {
   document.getElementById('result').innerHTML = winner + "wins."
+  document.getElementById('userChoice').innerHTML = "User choice: " + userChoice
+  document.getElementById('computerChoice').innerHTML = "Computer choice: " + computerChoice
 }
 
 function getWinner(userChoice, computerChoice) {
   if ((userChoice == 'rock' && computerChoice == 'scissors') || (userChoice == 'paper' && computerChoice == 'rock') || (userChoice == 'scissors' && computerChoice == 'paper')) {
        // userWin();
-       displayResult('User ')
+       displayResult('User ', userChoice, computerChoice)
      } else if (userChoice == computerChoice) {
        // tieGame();
-       displayResult('Tie. Neither ')
+       displayResult('Tie. Nobody ', userChoice, computerChoice)
      } else {
-       displayResult('Computer ');
+       displayResult('Computer ', userChoice, computerChoice);
     }
 }
 
 function startGame(e) {
   var userChoice = e.target.id
-  console.log(userChoice);
   var computerChoice = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)]
-  console.log(computerChoice);
+  showComputerChoiceImg(computerChoice)
   getWinner(userChoice, computerChoice)
 }
 
