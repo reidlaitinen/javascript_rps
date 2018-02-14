0f7a6eb700b1903a6bdbcbe9a399ce76a370a124
Reid Laitinen <reidlaitinen@gmail.com>
2018-02-13 17:16:14 -0700

    outputs winner to an h1 tag on page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
-function startGame(e) {
-  var choice = e.target.id
-  //debugger //won't pause unless dev tools are open
-  console.log(choice);
+function tieGame() {
+  console.log("tie game")
 }
 
+function userWin() {
+  console.log("you win!")
+}
 
+function computerWin() {
+  console.log("you lose!")
+}
+
+function displayResult(winner) {
+  document.getElementById('result').innerHTML = winner + "wins."
+}
+
+function getWinner(userChoice, computerChoice) {
+  if ((userChoice == 'rock' && computerChoice == 'scissors') || (userChoice == 'paper' && computerChoice == 'rock') || (userChoice == 'scissors' && computerChoice == 'paper')) {
+       // userWin();
+       displayResult('User ')
+     } else if (userChoice == computerChoice) {
+       // tieGame();
+       displayResult('Tie. Neither ')
+     } else {
+       displayResult('Computer ');
+    }
+}
+
+function startGame(e) {
+  var userChoice = e.target.id
+  console.log(userChoice);
+  var computerChoice = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)]
+  console.log(computerChoice);
+  getWinner(userChoice, computerChoice)
+}
 
 var choices = document.getElementsByClassName('choice')
 for (var i in choices) {
